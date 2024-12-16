6b416ca957ecd9ba3941379c2caa6c51ff79de56
Marta en Mac <mvigara79@gmail.com>
2024-12-16 13:18:48 +0100

    feat: implement optimized push_back and sort_medium
    push_back.c:
    - Add efficient number return system to stack A
    - Implement double rotation optimization
    - Use t_move structure for cost management
    - Remove index dependency
    
    sort_medium.c:
    - Add bifurcated approach for 4-100 numbers
    - Optimize small number cases (≤5)
    - Implement efficient strategy for 6-100 numbers
    - Special optimization for 5 numbers case
    
    Related: #push_swap-optimization

--- src/sort/push_back.c
+++ src/sort/push_back.c
@@ -6,7 +6,7 @@
 /*   By: mvigara- <mvigara-@student.42school.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 04:51:12 by mvigara-          #+#    #+#             */
-/*   Updated: 2024/12/15 05:26:00 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/12/16 13:15:28 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,85 +14,83 @@
 
 static void    execute_rotation(t_stack *stack, int cost, char stack_name)
 {
-    while (cost > 0)
+    while (cost)
     {
-        if (stack_name == 'a')
-            ra(stack);
+        if (cost > 0)
+        {
+            if (stack_name == 'a')
+                ra(stack);
+            else
+                rb(stack);
+            cost--;
+        }
         else
-            rb(stack);
-        cost--;
+        {
+            if (stack_name == 'a')
+                rra(stack);
+            else
+                rrb(stack);
+            cost++;
+        }
     }
 }
 
-static void    execute_rev_rotation(t_stack *stack, int cost, char stack_name)
+static void    execute_double_moves(t_stack *a, t_stack *b, t_move *move)
 {
-    while (cost > 0)
+    while (move->cost_a && move->cost_b && 
+           ((move->cost_a > 0 && move->cost_b > 0) || 
+            (move->cost_a < 0 && move->cost_b < 0)))
     {
-        if (stack_name == 'a')
-            rra(stack);
-        else
-            rrb(stack);
-        cost--;
-    }
-}
-
-static void    execute_move(t_stack *a, t_stack *b, t_move *move)
-{
-    // Ejecutar rotaciones dobles primero si es posible
-    while (move->rev_a == move->rev_b && move->cost_a > 0 && move->cost_b > 0)
-    {
-        if (move->rev_a)
-            rrr(a, b);
-        else
+        if (move->cost_a > 0)
+        {
             rr(a, b);
-        move->cost_a--;
-        move->cost_b--;
+            move->cost_a--;
+            move->cost_b--;
+        }
+        else
+        {
+            rrr(a, b);
+            move->cost_a++;
+            move->cost_b++;
+        }
     }
-
-    // Completar rotaciones individuales restantes
-    if (move->rev_a)
-        execute_rev_rotation(a, move->cost_a, 'a');
-    else
-        execute_rotation(a, move->cost_a, 'a');
-
-    if (move->rev_b)
-        execute_rev_rotation(b, move->cost_b, 'b');
-    else
-        execute_rotation(b, move->cost_b, 'b');
-
-    // Realizar el push
+    execute_rotation(a, move->cost_a, 'a');
+    execute_rotation(b, move->cost_b, 'b');
     pa(a, b);
+}
+
+static void    find_best_move(t_stack *a, t_stack *b, t_move *best)
+{
+    t_node  *current;
+    t_move  tmp;
+    int     found_better;
+
+    current = b->top;
+    found_better = 0;
+    while (current)
+    {
+        init_move(&tmp);
+        tmp.value = current->value;
+        calculate_move_cost(a, b, &tmp);
+
+        if (!found_better || tmp.total_cost < best->total_cost)
+        {
+            *best = tmp;
+            found_better = 1;
+        }
+        current = current->next;
+    }
 }
 
 void    push_back(t_stack *a, t_stack *b)
 {
-    t_move  current;
     t_move  best;
-    t_node  *tmp;
-    int     found_better;
 
     while (b->size > 0)
     {
-        found_better = 0;
-        tmp = b->top;
         init_move(&best);
         best.total_cost = INT_MAX;
-
-        while (tmp)
-        {
-            init_move(&current);
-            current.value = tmp->value;
-            calculate_move_cost(a, b, &current);
-
-            if (current.total_cost < best.total_cost)
-            {
-                best = current;
-                found_better = 1;
-            }
-            tmp = tmp->next;
-        }
-
-        if (found_better)
-            execute_move(a, b, &best);
+        find_best_move(a, b, &best);
+        execute_double_moves(a, b, &best);
     }
 }
--- src/sort/sort_medium.c
+++ src/sort/sort_medium.c
@@ -6,61 +6,76 @@
 /*   By: mvigara- <mvigara-@student.42school.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 20:19:17 by mvigara-          #+#    #+#             */
-/*   Updated: 2024/12/15 13:29:37 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/12/16 13:16:41 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	move_min_to_b(t_stack *stack_a)
+static void move_min_to_b(t_stack *stack_a)
 {
-	int	min_pos;
-	int	size;
+    int min_pos;
+    int size;
 
-	min_pos = get_min_pos(stack_a);
-	size = stack_a->size;
-	while (min_pos != 0)
-	{
-		if (min_pos <= size / 2)
-			ra(stack_a);
-		else
-			rra(stack_a);
-		min_pos = get_min_pos(stack_a);
-	}
+    min_pos = get_min_pos(stack_a);
+    size = stack_a->size;
+    while (min_pos != 0)
+    {
+        if (min_pos <= size / 2)
+            ra(stack_a);
+        else
+            rra(stack_a);
+        min_pos = get_min_pos(stack_a);
+    }
 }
 
-static void	first_to_b(t_stack *a, t_stack *b)
+static void first_to_b(t_stack *a, t_stack *b)
 {
-	int	i;
-	int	target_size;
+    int i;
+    int target_size;
 
-	if (!a || !b)
-		return ;
-	target_size = (a->size <= 5) ? 3 : (a->size - 3);
-	i = 0;
-	while (i < target_size && a->size > 3)
-	{
-		if (a->size <= 5)
-			move_min_to_b(a);
-		pb(a, b);
-		i++;
-	}
-	if (b && b->size == 2 && b->top->value < b->top->next->value)
-		sb(b);
+    if (!a || !b)
+        return ;
+    
+    // Para ≤5 números, dejamos 3 en A. Para >5, dejamos solo los 3 mayores
+    target_size = (a->size <= 5) ? 3 : (a->size - 3);
+    i = 0;
+    while (i < target_size && a->size > 3)
+    {
+        if (a->size <= 5)
+            move_min_to_b(a);
+        pb(a, b);
+        i++;
+    }
+    
+    // Para 5 números, optimizamos B si es necesario
+    if (b && b->size == 2 && b->top->value < b->top->next->value)
+        sb(b);
 }
 
-void	sort_medium(t_stack *a, t_stack *b)
+void sort_medium(t_stack *a, t_stack *b)
 {
-	if (!a || !b)
-		return ;
-	first_to_b(a, b);
-	sort_three(a, 'a');
-	if (a->size <= 5)
-	{
-		while (b && b->size > 0)
-			pa(a, b);
-	}
-	else
-		push_back(a, b);
-	rotate_to_min(a);
+    if (!a || !b)
+        return ;
+
+    // Primero movemos números a B dejando 3 en A
+    first_to_b(a, b);
+
+    // Ordenamos los 3 números en A
+    sort_three(a, 'a');
+
+    // Para ≤5 números usamos una estrategia simple
+    if (a->size <= 5)
+    {
+        while (b && b->size > 0)
+            pa(a, b);
+    }
+    else
+    {
+        // Para >5 números usamos push_back optimizado
+        push_back(a, b);
+    }
+
+    // Aseguramos que el mínimo quede arriba
+    rotate_to_min(a);
 }
