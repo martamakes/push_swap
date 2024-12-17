ba00f361cc793fe0c9a8a983e000518a21f7c8ca
Marta en Mac <mvigara79@gmail.com>
2024-12-17 11:39:04 +0100

    feat: Initial implementation of main and stack utils
    - Added main program structure with argument processing
    - Implemented basic stack utilities:
      - stack_new: Create new stack node
      - stack_last: Get last element
      - stack_add_back: Add element to end
      - stack_size: Count elements
      - free_stack: Free all memory

--- src/main/push_swap.c
+++ src/main/push_swap.c
@@ -6,7 +6,84 @@
 /*   By: mvigara- <mvigara-@student.42school.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 15:44:33 by mvigara-          #+#    #+#             */
-/*   Updated: 2024/12/15 13:28:45 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/12/17 11:36:15 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+static int	is_sorted(t_stack *stack)
+{
+	if (!stack)
+		return (1);
+	while (stack->next)
+	{
+		if (stack->value > stack->next->value)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+static void	push_swap(t_stack **stack_a, t_stack **stack_b, int total_nodes)
+{
+	if (total_nodes == 2 && !is_sorted(*stack_a))
+		sa(stack_a);
+	else if (total_nodes == 3)
+		sort_three(stack_a);
+	else if (total_nodes > 3 && !is_sorted(*stack_a))
+		sort_stack(stack_a, stack_b);
+}
+
+static void	get_numbers(char *av, t_stack **stack_a)
+{
+	char		**numbers;
+	long		num;
+	int			i;
+
+	numbers = ft_split(av, ' ');
+	if (!numbers)
+		error_exit(stack_a, NULL);
+	i = 0;
+	while (numbers[i])
+	{
+		if (!is_valid_input(numbers[i]))
+		{
+			free_split(numbers);
+			error_exit(stack_a, NULL);
+		}
+		num = ft_atoll(numbers[i]);
+		if (num > INT_MAX || num < INT_MIN)
+		{
+			free_split(numbers);
+			error_exit(stack_a, NULL);
+		}
+		stack_add_back(stack_a, stack_new((int)num));
+		free(numbers[i++]);
+	}
+	free(numbers);
+}
+
+int	main(int ac, char **av)
+{
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	int		total_nodes;
+	int		i;
+
+	if (ac < 2)
+		return (0);
+	stack_a = NULL;
+	stack_b = NULL;
+	i = 1;
+	while (i < ac)
+		get_numbers(av[i++], &stack_a);
+	if (!stack_a || check_duplicates(stack_a))
+		error_exit(&stack_a, NULL);
+	total_nodes = stack_size(stack_a);
+	if (!is_sorted(stack_a))
+		push_swap(&stack_a, &stack_b, total_nodes);
+	free_stack(&stack_a);
+	free_stack(&stack_b);
+	return (0);
+}
--- src/stack/stack_utils.c
+++ src/stack/stack_utils.c
@@ -6,8 +6,77 @@
 /*   By: mvigara- <mvigara-@student.42school.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 11:10:18 by mvigara-          #+#    #+#             */
-/*   Updated: 2024/12/05 14:56:24 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/12/17 11:37:43 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+t_stack	*stack_new(int value)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof(t_stack));
+	if (!new)
+		return (NULL);
+	new->value = value;
+	new->index = 0;
+	new->pos = -1;
+	new->target_pos = -1;
+	new->cost_a = -1;
+	new->cost_b = -1;
+	new->next = NULL;
+	return (new);
+}
+
+t_stack	*stack_last(t_stack *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack->next != NULL)
+		stack = stack->next;
+	return (stack);
+}
+
+void	stack_add_back(t_stack **stack, t_stack *new)
+{
+	t_stack	*last;
+
+	if (!new)
+		return ;
+	if (!*stack)
+	{
+		*stack = new;
+		return ;
+	}
+	last = stack_last(*stack);
+	last->next = new;
+}
+
+int	stack_size(t_stack *stack)
+{
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
+}
+
+void	free_stack(t_stack **stack)
+{
+	t_stack	*tmp;
+
+	if (!stack || !(*stack))
+		return ;
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+	*stack = NULL;
+}
