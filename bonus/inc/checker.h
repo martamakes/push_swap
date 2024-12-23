899621b80eae01f6976cbefbe242c8b9e02e038a
Marta en Mac <mvigara79@gmail.com>
2024-12-23 18:49:51 +0100

    created first version of bonus. Works fine up to 50 numbers. swaps correctly up to 150, but with toomany ops. KO for more than 150 ops

--- bonus/inc/checker.h
+++ bonus/inc/checker.h
@@ -6,7 +6,7 @@
 /*   By: mvigara- <mvigara-@student.42school.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 08:26:16 by mvigara-          #+#    #+#             */
-/*   Updated: 2024/12/20 09:43:39 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/12/21 21:23:04 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,42 @@
 # define CHECKER_H
 
 # include "../../inc/push_swap.h"
-# include <unistd.h>
-# include <stdlib.h>
+# include "../../lib/libft/inc/libft.h"
 
-typedef struct s_log {
-    int     total_ops;
-    int     valid_ops;
-    int     invalid_ops;
-    char    *last_op;
-    int     debug_mode;
-} t_log;  
+typedef struct s_log
+{
+	int		total_ops;
+	int		valid_ops;
+	int		invalid_ops;
+	char	*last_op;
+	int		debug_mode;
+}			t_log;
 // Main checker functions
-int	execute_instruction(char *line, t_stack **a, t_stack **b, t_log *log);
-void    free_and_exit(t_stack **a, t_stack **b, int status);
+int			execute_instruction(char *line, t_stack **a, t_stack **b,
+				t_log *log);
+void		free_and_exit(t_stack **a, t_stack **b, int status);
 
 // Operations
-void    checker_sa(t_stack **a, t_log *log);
-void    checker_sb(t_stack **b, t_log *log);
-void    checker_ss(t_stack **a, t_stack **b, t_log *log);
-void    checker_pa(t_stack **a, t_stack **b, t_log *log);
-void    checker_pb(t_stack **a, t_stack **b, t_log *log);
-void    checker_ra(t_stack **a, t_log *log);
-void    checker_rb(t_stack **b, t_log *log);
-void    checker_rr(t_stack **a, t_stack **b, t_log *log);
-void    checker_rra(t_stack **a, t_log *log);
-void    checker_rrb(t_stack **b, t_log *log);
-void    checker_rrr(t_stack **a, t_stack **b, t_log *log);
+void		checker_sa(t_stack **a, t_log *log);
+void		checker_sb(t_stack **b, t_log *log);
+void		checker_ss(t_stack **a, t_stack **b, t_log *log);
+void		checker_pa(t_stack **a, t_stack **b, t_log *log);
+void		checker_pb(t_stack **a, t_stack **b, t_log *log);
+void		checker_ra(t_stack **a, t_log *log);
+void		checker_rb(t_stack **b, t_log *log);
+void		checker_rr(t_stack **a, t_stack **b, t_log *log);
+void		checker_rra(t_stack **a, t_log *log);
+void		checker_rrb(t_stack **b, t_log *log);
+void		checker_rrr(t_stack **a, t_stack **b, t_log *log);
 
 // Utils
-int     check_number(char *str);
-int     is_valid_number_str(char *str);
-void    print_stack(t_stack *stack, char stack_name);
+int			check_number(char *str);
+int			is_valid_number_str(char *str);
+void		print_stack(t_stack *stack, char stack_name);
 
 // Debug functions
-void    init_log(t_log *log);
-void    log_operation(t_log *log, const char *op);
-void    print_debug_info(t_stack *a, t_stack *b, t_log *log);
+void		init_log(t_log *log);
+void		log_operation(t_log *log, const char *op);
+void		print_debug_info(t_stack *a, t_stack *b, t_log *log);
 
 #endif
