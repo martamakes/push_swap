a2a736431f5ce185feab7087276b4c5c4b3df1fe
Marta en Mac <mvigara79@gmail.com>
2024-12-23 18:51:02 +0100

    updated headers

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,17 +6,17 @@
 /*   By: mvigara- <mvigara-@student.42school.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 15:28:20 by mvigara-          #+#    #+#             */
-/*   Updated: 2024/12/19 15:32:59 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/12/23 18:43:53 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
 # include "../lib/libft/inc/libft.h"
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # ifndef DEBUG
 #  define VISUALS 0
@@ -26,64 +26,64 @@
 
 typedef struct s_stack
 {
-    int             value;      // Valor numérico del nodo
-    int             index;      // Índice para ordenación
-    int             pos;        // Posición actual en el stack
-    int             target_pos; // Posición objetivo en el otro stack
-    int             cost_a;     // Coste de mover en stack a
-    int             cost_b;     // Coste de mover en stack b
-    struct s_stack  *next;      // Puntero al siguiente nodo
-}   t_stack;
+	int value;            // Valor numérico del nodo
+	int index;            // Índice para ordenación
+	int pos;              // Posición actual en el stack
+	int target_pos;       // Posición objetivo en el otro stack
+	int cost_a;           // Coste de mover en stack a
+	int cost_b;           // Coste de mover en stack b
+	struct s_stack *next; // Puntero al siguiente nodo
+}		t_stack;
 
 // Stack operations
-t_stack     *stack_new(int value);
-void        stack_add_back(t_stack **stack, t_stack *new);
-t_stack     *stack_last(t_stack *stack);
-int         stack_size(t_stack *stack);
+t_stack	*stack_new(int value);
+void	stack_add_back(t_stack **stack, t_stack *new);
+t_stack	*stack_last(t_stack *stack);
+int		stack_size(t_stack *stack);
 // Operations functions
-void        sa(t_stack **stack_a);
-void        sb(t_stack **stack_b);
-void        ss(t_stack **stack_a, t_stack **stack_b);
-void        pa(t_stack **stack_a, t_stack **stack_b);
-void        pb(t_stack **stack_b, t_stack **stack_a);
-void        ra(t_stack **stack_a);
-void        rb(t_stack **stack_b);
-void        rr(t_stack **stack_a, t_stack **stack_b);
-void        rra(t_stack **stack_a);
-void        rrb(t_stack **stack_b);
-void        rrr(t_stack **stack_a, t_stack **stack_b);
+void	sa(t_stack **stack_a);
+void	sb(t_stack **stack_b);
+void	ss(t_stack **stack_a, t_stack **stack_b);
+void	pa(t_stack **stack_a, t_stack **stack_b);
+void	pb(t_stack **stack_b, t_stack **stack_a);
+void	ra(t_stack **stack_a);
+void	rb(t_stack **stack_b);
+void	rr(t_stack **stack_a, t_stack **stack_b);
+void	rra(t_stack **stack_a);
+void	rrb(t_stack **stack_b);
+void	rrr(t_stack **stack_a, t_stack **stack_b);
 
 // Input processing and validation
-int         is_valid_input(char *str);
-int         check_duplicates(t_stack *stack);
-int         process_input(int ac, char **av, t_stack **stack_a);
-void        ft_split_free(char **split);
+int		is_valid_input(char *str);
+int		check_duplicates(t_stack *stack);
+int		process_input(int ac, char **av, t_stack **stack_a);
+void	ft_split_free(char **split);
 
 // Error handling
-void        error_exit(t_stack **stack_a, t_stack **stack_b);
+void	error_exit(t_stack **stack_a, t_stack **stack_b);
 
 // Sorting algorithms
-void        sort_three(t_stack **stack);
-void        sort_stack(t_stack **stack_a, t_stack **stack_b);
+void	sort_three(t_stack **stack);
+void	sort_stack(t_stack **stack_a, t_stack **stack_b);
 
 // Utils
-int         is_sorted(t_stack *stack);
-void        free_stack(t_stack **stack);
-int         get_min_pos(t_stack *stack);
-void do_rotations(t_stack **a, t_stack **b, int cost_a, int cost_b);
-void turkish_sort(t_stack **a, t_stack **b);
-void    get_cost(t_stack *a, t_stack *b);
-void    get_target_positions(t_stack *a, t_stack *b);
-void        sort_small(t_stack **a, t_stack **b, int size);
-t_stack     *get_cheapest(t_stack *stack);
-void    move_cheapest_to_a(t_stack **a, t_stack **b);
-void        move_cheapest_to_b(t_stack **a, t_stack **b);
-void        shift_stack(t_stack **stack);
-int find_target_position(t_stack *a, t_stack *b, int value);
+int		is_sorted(t_stack *stack);
+void	free_stack(t_stack **stack);
+int		get_min_pos(t_stack *stack);
+void	do_rotations(t_stack **a, t_stack **b, int cost_a, int cost_b);
+void	turkish_sort(t_stack **a, t_stack **b);
+void	get_cost(t_stack *a, t_stack *b);
+void	get_target_positions(t_stack *a, t_stack *b);
+void	sort_small(t_stack **a, t_stack **b, int size);
+t_stack	*get_cheapest(t_stack *stack);
+void	move_cheapest_to_a(t_stack **a, t_stack **b);
+void	move_cheapest_to_b(t_stack **a, t_stack **b);
+void	shift_stack(t_stack **stack);
+int		find_target_position(t_stack *a, t_stack *b, int value);
 
-//debug
-void print_stacks(t_stack *stack_a, t_stack *stack_b);
-void    print_one(t_stack *stack, char stack_name);
+// debug
+void	print_stacks(t_stack *stack_a, t_stack *stack_b);
+void	print_one(t_stack *stack, char stack_name);
 void	print_debug_str(char *str);
 
 #endif
